09fde2cfccb424fbe5f215c6d2a0f4ce47e9b509
Ben Slater <bas58@cam.ac.uk>
2026-09-11 14:16:16 +0100

    Update build and website for dev resources

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  S3_BASE_URL: 'https://animal-ai-website-experiment-data-storage.s3.eu-north-1.amazonaws.com',
-  LIFECYCLE_LAMBDA_URL: 'https://ovqx9h40zg.execute-api.eu-north-1.amazonaws.com/test/telemetry',
-  FEEDBACK_UPLOAD_LABMDA_URL: 'https://x314h4yzq5.execute-api.eu-north-1.amazonaws.com/Prod/telemetry'
+  S3_BASE_URL: 'https://test-experiment-data-storage.s3.eu-north-1.amazonaws.com',
+  LIFECYCLE_LAMBDA_URL: 'https://ijxyyr92x3.execute-api.eu-north-1.amazonaws.com/test/telemetry',
+  FEEDBACK_UPLOAD_LABMDA_URL: 'https://mjlo2ftcn3.execute-api.eu-north-1.amazonaws.com/Prod/telemetry'
 };
